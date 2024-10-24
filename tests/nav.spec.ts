7ec0db7cb32e7b88f36d6a58106eaab6c1e410a1
ofindley <orane.findley@gmail.com>
2024-10-23 22:50:41 -0500

    added tests to navigate across multiple sections

--- tests/nav.spec.ts
+++ tests/nav.spec.ts
@@ -73,4 +73,33 @@ test.describe("Navigation", () => {
       sections.contactSection.title
     );
   });
+
+  test("Verify navigation to multiple sections [About, Experience, Skills, Projects, Contact]", async ({
+    page,
+  }) => {
+    // Navigate to about section
+    await navBar.aboutBtn.click();
+    expect(page.url()).toContain(urls.aboutUrl);
+    await expect(mainPage.aboutSection).toBeInViewport();
+
+    // Navigate to experience section
+    await navBar.experienceBtn.click();
+    expect(page.url()).toContain(urls.experienceUrl);
+    await expect(mainPage.experienceSection).toBeInViewport();
+
+    // Navigate to skills section
+    await navBar.skillsBtn.click();
+    expect(page.url()).toContain(urls.skillsUrl);
+    await expect(mainPage.skillsSection).toBeInViewport();
+
+    // Navigate to projects section
+    await navBar.projectsBtn.click();
+    expect(page.url()).toContain(urls.projectsUrl);
+    await expect(mainPage.projectsSection).toBeInViewport();
+
+    // Navigate to contact section
+    await navBar.contactBtn.click();
+    expect(page.url()).toContain(urls.contactUrl);
+    await expect(mainPage.contactSection).toBeInViewport();
+  });
 });
