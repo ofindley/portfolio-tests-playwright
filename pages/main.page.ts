1121fc750cd9bcfb7ef165410bce15401109dfb5
ofindley <orane.findley@gmail.com>
2024-10-25 17:50:00 -0500

    added page elements

--- pages/main.page.ts
+++ pages/main.page.ts
@@ -5,8 +5,12 @@ export class MainPage {
   readonly page: Page;
 
   readonly favicon: Locator;
+  readonly html: Locator;
 
+  // Hero section
   readonly heroSection: Locator;
+  readonly profilePic: Locator;
+
   readonly aboutSection: Locator;
   readonly experienceSection: Locator;
   readonly skillsSection: Locator;
@@ -19,25 +23,39 @@ export class MainPage {
   readonly emailBtn: Locator;
 
   readonly scrollToTopBtn: Locator;
+  readonly themeToggleBtn: Locator;
 
   constructor(page: Page) {
     this.page = page;
 
     this.favicon = page.locator('link[rel="icon"]');
+    this.html = page.locator("html");
 
+    // Hero section
     this.heroSection = page.locator(".hero");
-    this.aboutSection = page.locator("#about");
-    this.experienceSection = page.locator("#experience");
-    this.skillsSection = page.locator("#skills");
-    this.projectsSection = page.locator("#projects");
-    this.contactSection = page.locator("#contact");
+    this.profilePic = this.heroSection.locator("img");
 
-    this.linkedinBtn = page.locator("#contact a[aria-label='LinkedIn']");
-    this.githubBtn = page.locator("#contact a[aria-label='GitHub']");
-    this.twitterBtn = page.locator("#contact a[aria-label='Twitter']");
-    this.emailBtn = page.locator("#contact-me");
+    // About sections
+    this.aboutSection = page.locator("#about");
+
+    // Experience section
+    this.experienceSection = page.locator("#experience");
+
+    // Skills section
+    this.skillsSection = page.locator("#skills");
+
+    // Projects section
+    this.projectsSection = page.locator("#projects");
+
+    // Contact section
+    this.contactSection = page.locator("#contact");
+    this.linkedinBtn = this.contactSection.locator("a[aria-label='LinkedIn']");
+    this.githubBtn = this.contactSection.locator("a[aria-label='GitHub']");
+    this.twitterBtn = this.contactSection.locator("a[aria-label='Twitter']");
+    this.emailBtn = this.contactSection.locator("#contact-me");
 
     this.scrollToTopBtn = page.locator("#scroll-to-top");
+    this.themeToggleBtn = page.locator("#theme-toggle");
   }
 
   async navigateToSection(section: string) {
@@ -77,5 +95,38 @@ export class MainPage {
 
     await this.verifyElementsHaveText(headers, "Header");
     await this.verifyElementsHaveText(paragraphs, "Paragraph");
+  }
+
+  async verifyTagHasClass(
+    section: string,
+    tag: string,
+    className: string
+  ): Promise<void> {
+    const elements = await this[`${section}Section`].locator(tag).all();
+    expect(
+      elements.length,
+      `No ${tag} elements found in the ${section} section`
+    ).toBeGreaterThan(0);
+
+    for (let i = 0; i < elements.length; i++) {
+      const hasClass = await elements[i].evaluate(
+        (el, className) => el.classList.contains(className),
+        className
+      );
+      expect(
+        hasClass,
+        `${tag} element at index ${i} in the ${section} section has '${className}' class`
+      ).toBeTruthy();
+    }
+  }
+
+  async setLightTheme() {
+    const isDark = await this.html.evaluate((el) =>
+      el.classList.contains("dark")
+    );
+    if (isDark) {
+      await this.themeToggleBtn.click();
+      await expect(this.html).not.toHaveClass(/dark/);
+    }
   }
 }
